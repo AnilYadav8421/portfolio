46d7732fa77e19227667249623112e4028b4df03
Anil Yadav <anilyadav8149838058@gmail.com>
2025-05-14 18:21:55 +0530

    Navbar & aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import Home from "./pages/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NotFound from "./pages/NotFound"
 const App = () => {
   return (
-    <div>App</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
